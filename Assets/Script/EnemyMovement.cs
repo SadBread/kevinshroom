25459c05f0288a18eb73b4a5d31bd2fb719fbae0
rakellundstrom <rakel.lundstrom@hotmail.se>
2024-09-18 22:28:57 +0200

    start projekt

---
+++ Assets/Script/EnemyMovement.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 2.0f;
+    [SerializeField] private float bounciness = 1000;
+    [SerializeField] private float knockbackForce = 200f;
+    [SerializeField] private float upwardForce = 100f;
+    [SerializeField] private int damageGiven = 1;
+    private SpriteRenderer rend;
+
+    private void Start()
+    {
+        rend = GetComponent<SpriteRenderer>();
+    }
+    void FixedUpdate()
+    {
+        transform.Translate(new Vector2 (moveSpeed, 0) * Time.deltaTime);
+
+        if(moveSpeed > 0)
+        {
+            rend.flipX = false;
+        }
+
+        if(moveSpeed < 0)
+        {
+            rend.flipX = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("EnemyBlock"))
+        {
+            moveSpeed = -moveSpeed;
+        }
+
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            moveSpeed = -moveSpeed;
+        }
+
+        if(other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<PlayerMovement>().TakeDamage(damageGiven);
+
+            if(other.transform.position.x > transform.position.x)
+            {
+                other.gameObject.GetComponent<PlayerMovement>().TakeKnockBack(knockbackForce, upwardForce);
+            }
+            else
+            {
+                other.gameObject.GetComponent<PlayerMovement>().TakeKnockBack(-knockbackForce, upwardForce);
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<Rigidbody2D>().velocity = new Vector2(other.GetComponent<Rigidbody2D>().velocity.x, 0);
+            other.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, bounciness));
+            Destroy(gameObject);
+        }
+    }
+}
